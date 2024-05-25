9e6f29ced2b18d0768f6d3a2a41a8550aae917d3
lequanghai <lequanghai.private@gmail.com>
2024-05-26 02:07:16 +0700

    Re-create backend asp app

--- backend/Program.cs
+++ backend/Program.cs
@@ -9,6 +9,7 @@ internal class Program
         builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
+
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
